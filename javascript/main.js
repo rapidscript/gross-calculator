52c86ec82aa9be2f0773dcbb6cba00d5717964ab
ChelseaKerubo <chelseakerubo1@gmail.com>
2022-02-22 12:25:13 +0300

    Added  dark theme

--- javascript/main.js
+++ javascript/main.js
@@ -6,3 +6,10 @@ function calculateGrossSalary() {
     const TRANSPORT_ALLOWANCE = 5000;
     var gross =  base.value + houseAllowance + TRANSPORT_ALLOWANCE;
 }
+
+/* Change the theme of the website from a light theme to a dark theme */
+
+function darktheme(){
+    var element=document.body;
+    element.classList.toggle("dark-mode");
+}
